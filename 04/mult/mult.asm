607ce8ff4304d40c8c462bd83020a15de65e193a
kerojosh96 <jaybirdcook@gmail.com>
2019-02-18 13:02:34 -0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,38 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R0
+D=M
+@R1
+D=M
+@COUNT
+M=D
+@R2
+M=0
+
+//Decrementing counter
+(LOOP)
+@COUNT
+D=M
+@END
+D;JEQ
+@COUNT
+M=D-1
+
+//Incrementing
+@R0 // ex 3
+D=M 
+@R2
+M=D+M
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
+
+
+
+
+
+
