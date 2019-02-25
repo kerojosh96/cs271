f37e43196f043f97db763de0619c38c6b3f3a1d6
kerojosh96 <jaybirdcook@gmail.com>
2019-02-24 17:20:05 -0800

    Update Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,39 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+ // A stores location and M stores Value
+
+(RESTART)
+@SCREEN // is a number like 18903 somthing 
+D=A             // loads 18900 into D
+@Location                                   // is a varible 
+M=D            // Stores the value of SCREEN in Memory
+(LOOP)
+@KBD          
+D=A           // loads keybord input into D
+@Location     
+D=D-M         // This subtracts the keybord from location
+@RESTART      
+D;JEQ         // RESTARTs the loop
+@Location    
+A=M          
+M=0         // sets location = 0
+@KBD        
+D=M         // loads KBD into M
+@BLACK      
+D;JGT       // if less than 0 goto black
+
+
+@Increment  
+0;JMP       // jumps to Increment
+
+(BLACK)
+@Location
+A=M      // loads location and sets Memory = A
+M=-1
+(Increment)
+@Location // Loads Location 
+M=M+1 // Increments it by 1
+
+@LOOP
+0;JMP //loops
